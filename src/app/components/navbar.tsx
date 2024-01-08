0509833a5f0e7988b659531e63439c1fa65e17f6
devPesto <pestonotpasta@gmail.com>
2024-01-08 10:21:39 -0800

    Add navbar styling on active pages

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <nav className="sticky top-0 text-white bg-triton  flex items-center px-10 py-5 shadow-lg z-10">
             <Image
@@ -10,23 +15,34 @@ export default function Navbar() {
                 height={75}
                 alt="Triton Logo"
             />
+
             <div className="flex text-md font-semibold ml-auto gap-5 justify-between">
-                <NavLink name="Home" href="/" />
-                <NavLink name="Tools" href="/" />
-                <NavLink name="Events" href="/" />
-                <NavLink name="Contact" href="/" />
+                {[
+                    ["Home", "/"],
+                    ["Tools", "/tools"],
+                    ["Events", "/events"],
+                    ["Contact", "/contact"],
+                ].map(([name, href]) => {
+                    return (
+                        <NavLink href={href} name={name} current={pathname} />
+                    );
+                })}
             </div>
         </nav>
     );
 }
 
-const NavLink = ({ name, href }: NavLinkProps) => (
-    <Link id={href} href={href} className="text-gray-300 hover:text-white">
-        {name}
-    </Link>
-);
+function NavLink({ name, href, current }: NavLinkProps) {
+    const styles = current === href ? "text-white" : "text-gray-300";
+    return (
+        <Link href={href} className={styles}>
+            {name}
+        </Link>
+    );
+}
 
 interface NavLinkProps {
     name: string;
     href: string;
+    current: string;
 }
