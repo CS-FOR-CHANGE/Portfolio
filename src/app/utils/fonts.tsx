2f814226b8640b5ab1faa8d4cf1670f1c30623d8
devPesto <pestonotpasta@gmail.com>
2024-04-17 17:03:11 -0700

    Example commit 2

--- src/app/utils/fonts.tsx
+++ src/app/utils/fonts.tsx
@@ -2,7 +2,7 @@ import { Poppins, Inter } from "next/font/google";
 
 export const poppins = Poppins({
     subsets: ["latin"],
-    weight: "700",
+    weight: "600",
     variable: "--poppins-font",
 });
 
